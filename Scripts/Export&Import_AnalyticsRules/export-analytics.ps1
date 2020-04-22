77050a61bcf9391a642ff1cd6c520158c4cfff60
Yaniv Shasha <yanivsh@microsoft.com>
2020-04-22 09:44:19 +0300

    commit

--- Scripts/Export&Import_AnalyticsRules/export-analytics.ps1
+++ Scripts/Export&Import_AnalyticsRules/export-analytics.ps1
@@ -1,23 +1,21 @@
 Login-AzAccount
 Import-Module AzSentinel
 
-
 #variables
-$outputFolder # Export folder
-$WorkspaceName = "CxP-AzureSecurityWS"
-$SubscriptionId = "6b1ceacd-5731-4780-8f96-2078dd96fd96"
+$outputFolder = "c:\export\"
+$WorkspaceName = "ImportWorkSpaceID"
+$SubscriptionId = "6b1ceacd-xxxx-xxxx-xxxx-2078dd96fd96"
 
 
 $alerts = Get-AzSentinelAlertRule -SubscriptionId $SubscriptionId -WorkspaceName $WorkspaceName
 
 foreach ($alert in $alerts)
 {
-If($alert.enabled -eq $True -and $alert.name -ne "FusionBuiltIn" -and $alert.name -ne "FusionBuiltIn" and $alert.severity -ne "null" )
+If($alert.enabled -eq $True -and $alert.name -ne "FusionBuiltIn" -and $alert.severity -ne "null")
 {
-   ## $alert | ConvertTo-Json | Out-File c:\temp\$random.name.yaml  
-   $outputFolder = "c:\temp\export\2\"
+  
     $outputFileName = $alert.name +".yaml"
     $exportlocation = $outputFolder + "\" + $outputFileName
+    $alert | ConvertTo-Json | Out-File $exportlocation  
 }
-$alert | ConvertTo-Json | Out-File $exportlocation  
 }
--- Scripts/Export&Import_AnalyticsRules/import-analytics.ps1
+++ Scripts/Export&Import_AnalyticsRules/import-analytics.ps1
@@ -1,8 +1,8 @@
 Login-AzAccount
 #variables
-$InputFolder = "c:\temp\export\1\" # Input folder
-$WorkspaceName = "CentricDemo"
-$SubscriptionId = "8f153238-e602-427e-a7c0-3043fbe50918"
+$InputFolder = "c:\import\" # Input folder
+$WorkspaceName = "YourWorkspaceID"
+$SubscriptionId = "8f153238-xxxx-xxxx-xxxx-3043fbe50918"
 
 
 $inputalerts = Get-ChildItem -path $InputFolder -Recurse
